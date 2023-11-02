ecaaead7ab4fd45fb7ca5249eedc7e46e200d732
pablolopezx <145000433+pablolopezx@users.noreply.github.com>
2023-11-02 10:57:55 +0100

    Ejercicio 3 corrección

--- sprint2python/operaciones.py
+++ sprint2python/operaciones.py
@@ -1,18 +1,25 @@
-numero1 = input("numero1?")
-numero2 = input("numero2?")
-numero1= int(numero1)    
-numero2= int(numero2)
-suma= numero1+numero2
-resta= numero1-numero2
-multiplicacion= numero1*numero2
-if (numero2 == 0):
-    print("No se puede dividir por 0")
-else: 
-    division= numero1/numero2
-
-print("Operaciones básicas:" + "\n" +
-      "Suma:" + str(numero1) + "+" + str(numero2) + "=" + str(suma) + "\n" +
-      "Resta:"+ str(numero1) + "-" + str(numero2) + "=" + str(resta) + "\n" + 
-      "Multiplicación:"+ str(numero1) + "x" + str(numero2) + "=" + str(multiplicacion) + "\n" + 
-      "División:"+ str(numero1) + "/" + str(numero2) + "=" + str(division)
-)
+numero1=0
+numero2=1
+def suma(numero1,numero2):
+    return numero1+numero2
+def resta(numero1,numero2):
+    return numero1-numero2
+def multiplicacion(numero1,numero2):
+    return numero1*numero2
+def division(numero1,numero2):
+    if (numero2 == 0):
+        return print("No se puede dividir por 0")
+    else: 
+        return numero1/numero2
+#Esto es para evitar fallos en los ejercicios siguientes al importar
+if __name__ == "__main__":
+    numero1 = input("numero1?")
+    numero2 = input("numero2?")
+    numero1= int(numero1)    
+    numero2= int(numero2)
+    print("Operaciones básicas:" + "\n" +
+      "Suma:" + str(numero1) + "+" + str(numero2) + "=" + str(suma(numero1,numero2)) + "\n" +
+      "Resta:"+ str(numero1) + "-" + str(numero2) + "=" + str(resta(numero1,numero2)) + "\n" + 
+      "Multiplicación:"+ str(numero1) + "x" + str(numero2) + "=" + str(multiplicacion(numero1,numero2)) + "\n" + 
+      "División:"+ str(numero1) + "/" + str(numero2) + "=" + str(division(numero1,numero2))
+    )
